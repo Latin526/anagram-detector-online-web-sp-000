3066d857066357b763f4b4eb938f7ebb001c91cf
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-20 16:07:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,9 +1,11 @@
 # Your code goes here!
 class Anagram
    attr_accessor :word
+   
    def initialize(word)
     @word = word
   end
+  
    def match(collection)
     output = []
     collection.each_with_index do |el, idx|
